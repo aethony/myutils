55a31a9c5d352ec41c3d0c0bd8d8adea789bf1c6
user1 <user1@centos62.example.com>
2012-05-17 10:50:52 +0800

    update

--- setgitconfig.sh
+++ setgitconfig.sh
@@ -1,6 +1,25 @@
 #!/bin/bash -
 
-for l in color.status color.diff color.branch color.interactive color.grep color.ui color.showbranch
-do 
-    git config --system $l 'auto'
-done 
+EMAIL="$USER@$(hostname)"
+
+
+if [ "$UID" -eq "0" ]
+then
+    for l in color.status color.diff color.branch color.interactive color.grep color.ui color.showbranch
+    do 
+        git config --system $l 'auto'
+    done
+fi
+
+
+git config --global sendemail.confirm "auto"
+git config --global format.headers "Organization: git-foo"
+git config --global format.subjectprefix "CHANGE"
+git config --global format.suffix ".txt"
+git config --global format.numbered "auto"
+#git config --global format.to "user1@centos62.example.com"
+git config --global format.cc "$EMAIL"
+git config --global format.attach
+git config --global format.signoff "true"
+git config --global user.name "$USER"
+git config --global user.email "$EMAIL"
